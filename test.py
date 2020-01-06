950e44750ed53e2df8e40a366474c29a18971ff1
Yann Prudent <yann.prudent@masagroup.net>
2020-01-06 10:40:05 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,3 @@ from sample.py import *
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
-
-def test_raises_exception_on_non_string_arguments():
-    with pytest.raises(TypeError):
-        capital_case(9)
