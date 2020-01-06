656ef8a3dc16946576eb25cd7788a9567652a746
Yann Prudent <yann.prudent@masagroup.net>
2020-01-06 10:17:03 +0100

    Add sample code and test

---
+++ src/tests/test.py
@@ -0,0 +1,10 @@
+
+import pytest
+import sample.py
+
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_raises_exception_on_non_string_arguments():
+    with pytest.raises(TypeError):
+        capital_case(9)
